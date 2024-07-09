c70a41a845bbda29e618e0e44f5034e3cdd8114b
Wanderer0074348 <tanaymatta27@gmail.com>
2024-07-09 16:45:42 +0400

    Added declaration, mutability, scope and shadowing concepts to main.rs

--- learning_rust/src/main.rs
+++ learning_rust/src/main.rs
@@ -1,7 +1,22 @@
 fn main() {
-    // Defination
+    // Definition
     let x:i8 = 10;
-    x = 5;
+    println!("{x}");
     // Mutability
+    let mut y:i16 = 1500;
+    println!("{y}");
+    y = 41;
+    println!("{y}");
 
+    //scope
+    {
+        let z:i32 = 5600000;
+        println!("{z}");
+    }
+
+    //shadowing
+    let t = 10;
+    println!("{t}");
+    let t = t+10;
+    println!("{t}");
 }
