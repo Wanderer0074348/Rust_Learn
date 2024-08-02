4bb049d848778576657657ce51e80f328e62adc3
Wanderer0074348 <tanaymatta27@gmail.com>
2024-08-02 11:10:47 +0400

    Created guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,23 +1,35 @@
+// use core::num;
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Enter a number to guess? ");
 
     let random_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("{}",random_number);
+    // println!("{}",random_number);
 
-    let mut value = String::new();
-
-    io::stdin().read_line(&mut value).expect("Expected to read line");
-
-    println!("you guessed {}", value);
-
-    if random_number.to_string() == value {
-        println!("Correct!");
-    }
-    else {
-        println!("incorrect!");
+    
+    
+    loop{
+        
+        let mut value = String::new();
+        io::stdin().read_line(&mut value).expect("Expected to read line");
+        let value:u32= match value.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        
+        println!("you guessed {}", value);
+        
+        match value.cmp(&random_number) {
+            Ordering::Less => println!("Too Small"),
+            Ordering::Greater => println!("Too Big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+        },
+        }
     }
 }
